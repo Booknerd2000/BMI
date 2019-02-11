bbf12bc924f14dea0e5882b17710e0a0c79729c5
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-11 07:47:09 -0500

    Asked BMI questions

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,12 +9,15 @@ public class Main {
 
 	keyboard = new Scanner(System.in);
 
+	System.out.println("What is your weight?");
 	weight = keyboard.nextInt();
 
+	System.out.println("What is your height(only feet)?");
 	feet = keyboard.nextInt();
 
+	System.out.println("What is your height(only inches)?");
 	inches = keyboard.nextInt();
 
-
+	
     }
 }
